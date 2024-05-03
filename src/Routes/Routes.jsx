4511d8088647afc368211decce0888d8564565af
alex11920 <av61799@gmail.com>
2024-05-04 00:21:22 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Controller/Root";
+import ErrorPage from "../Components/ErrorPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default router;
