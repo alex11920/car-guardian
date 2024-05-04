da3069694bc80afedb3ef3027d672a04354138fe
alex11920 <av61799@gmail.com>
2024-05-04 07:02:37 +0600

    about section done

---
+++ src/Components/About.jsx
@@ -0,0 +1,42 @@
+import person from "/src/assets/about_us/person.jpg";
+import parts from "/src/assets/about_us/parts.jpg";
+const About = () => {
+  return (
+    <div className="hero my-32">
+      <div className="hero-content flex-col lg:flex-row-reverse gap-24">
+        <div className="lg:w-1/2 shrink-0 text-center lg:text-left">
+          <h1 className="text-xl text-color font-bold">About Us</h1>
+          <h1 className="text-5xl font-bold py-5 max-w-96">
+            We are qualified & of experience in this field
+          </h1>
+          <p className="py-6 text-p max-w-[500px]">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomized words which do not look even slightly
+            believable.
+            <br />
+            <br />
+            The majority have suffered alteration in some form, by injected
+            humour, or randomized words which do not look even slightly
+            believable.
+          </p>
+          <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-[#FF3811]">
+            Discover More
+          </span>
+        </div>
+        <div className="card shrink-0 lg:w-1/2 items-center relative">
+          <img
+            src={person}
+            className="object-cover object-left w-[460px] h-[470px] rounded-xl"
+          />
+          <img
+            src={parts}
+            className="object-cover w-[250px] h-[250px] border-8 border-white shadow-lg rounded-xl absolute top-64 lg:left-72"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,9 +7,28 @@ import img6 from "/src/assets/banner/6.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full rounded-xl">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full" />
+        <div className="absolute h-full text-white bg-gradient-to-r from-black">
+          <div className="pl-6 pt-6 md:pl-40 md:pt-20 lg:pl-24 lg:pt-72">
+            <h2 className="text-2xl md:text-6xl lg:text-6xl text-center md:text-start lg:text-start font-bold max-w-lg">
+              Affordable Price For Car Servicing
+            </h2>
+            <p className="text-sm md:text-base lg:text-base text-center md:text-start lg:text-start max-w-lg py-8">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-6 justify-center md:justify-start lg:justify-start">
+              <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Discover More
+              </span>
+              <span className="btn btn-outline text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Latest Project
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-between top-1/2 lg:top-auto lg:justify-end transform -translate-y-1/2 left-5 right-5 lg:bottom-0 lg:mr-16 lg:mb-16">
           <a
             href="#slide6"
@@ -27,6 +46,25 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={img2} className="w-full" />
+        <div className="absolute h-full text-white bg-gradient-to-r from-black">
+          <div className="pl-6 pt-6 md:pl-40 md:pt-20 lg:pl-24 lg:pt-72">
+            <h2 className="text-2xl md:text-6xl lg:text-6xl text-center md:text-start lg:text-start font-bold max-w-96">
+              Affordable Price For Car Servicing
+            </h2>
+            <p className="text-sm md:text-base lg:text-base text-center md:text-start lg:text-start max-w-96 py-8">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-6 justify-center md:justify-start lg:justify-start">
+              <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Discover More
+              </span>
+              <span className="btn btn-outline text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Latest Project
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-between top-1/2 lg:top-auto lg:justify-end transform -translate-y-1/2 left-5 right-5 lg:bottom-0 lg:mr-16 lg:mb-16">
           <a
             href="#slide1"
@@ -44,6 +82,25 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={img3} className="w-full" />
+        <div className="absolute h-full text-white bg-gradient-to-r from-black">
+          <div className="pl-6 pt-6 md:pl-40 md:pt-20 lg:pl-24 lg:pt-72">
+            <h2 className="text-2xl md:text-6xl lg:text-6xl text-center md:text-start lg:text-start font-bold max-w-96">
+              Affordable Price For Car Servicing
+            </h2>
+            <p className="text-sm md:text-base lg:text-base text-center md:text-start lg:text-start max-w-96 py-8">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-6 justify-center md:justify-start lg:justify-start">
+              <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Discover More
+              </span>
+              <span className="btn btn-outline text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Latest Project
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-between top-1/2 lg:top-auto lg:justify-end transform -translate-y-1/2 left-5 right-5 lg:bottom-0 lg:mr-16 lg:mb-16">
           <a
             href="#slide2"
@@ -61,6 +118,25 @@ const Banner = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={img4} className="w-full" />
+        <div className="absolute h-full text-white bg-gradient-to-r from-black">
+          <div className="pl-6 pt-6 md:pl-40 md:pt-20 lg:pl-24 lg:pt-72">
+            <h2 className="text-2xl md:text-6xl lg:text-6xl text-center md:text-start lg:text-start font-bold max-w-96">
+              Affordable Price For Car Servicing
+            </h2>
+            <p className="text-sm md:text-base lg:text-base text-center md:text-start lg:text-start max-w-96 py-8">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-6 justify-center md:justify-start lg:justify-start">
+              <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Discover More
+              </span>
+              <span className="btn btn-outline text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Latest Project
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-between top-1/2 lg:top-auto lg:justify-end transform -translate-y-1/2 left-5 right-5 lg:bottom-0 lg:mr-16 lg:mb-16">
           <a
             href="#slide3"
@@ -78,6 +154,25 @@ const Banner = () => {
       </div>
       <div id="slide5" className="carousel-item relative w-full">
         <img src={img5} className="w-full" />
+        <div className="absolute h-full text-white bg-gradient-to-r from-black">
+          <div className="pl-6 pt-6 md:pl-40 md:pt-20 lg:pl-24 lg:pt-72">
+            <h2 className="text-2xl md:text-6xl lg:text-6xl text-center md:text-start lg:text-start font-bold max-w-96">
+              Affordable Price For Car Servicing
+            </h2>
+            <p className="text-sm md:text-base lg:text-base text-center md:text-start lg:text-start max-w-96 py-8">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-6 justify-center md:justify-start lg:justify-start">
+              <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Discover More
+              </span>
+              <span className="btn btn-outline text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Latest Project
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-between top-1/2 lg:top-auto lg:justify-end transform -translate-y-1/2 left-5 right-5 lg:bottom-0 lg:mr-16 lg:mb-16">
           <a
             href="#slide4"
@@ -95,6 +190,25 @@ const Banner = () => {
       </div>
       <div id="slide6" className="carousel-item relative w-full">
         <img src={img6} className="w-full" />
+        <div className="absolute h-full text-white bg-gradient-to-r from-black">
+          <div className="pl-6 pt-6 md:pl-40 md:pt-20 lg:pl-24 lg:pt-72">
+            <h2 className="text-2xl md:text-6xl lg:text-6xl text-center md:text-start lg:text-start font-bold max-w-96">
+              Affordable Price For Car Servicing
+            </h2>
+            <p className="text-sm md:text-base lg:text-base text-center md:text-start lg:text-start max-w-96 py-8">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-6 justify-center md:justify-start lg:justify-start">
+              <span className="btn bg-[#FF3811] border-[#FF3811] text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Discover More
+              </span>
+              <span className="btn btn-outline text-white hover:text-[#FF3811] hover:bg-white hover:border-white">
+                Latest Project
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-between top-1/2 lg:top-auto lg:justify-end transform -translate-y-1/2 left-5 right-5 lg:bottom-0 lg:mr-16 lg:mb-16">
           <a
             href="#slide5"
